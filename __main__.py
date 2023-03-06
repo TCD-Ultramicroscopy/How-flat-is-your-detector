888eb41c32f064dc5860e89663811bceff70eac2
Jonathan Peters <jonathanjppeters@gmail.com>
2023-03-06 11:46:31 +0000

    Added project files

---
+++ __main__.py
@@ -0,0 +1,75 @@
+# todo: make scipy import explicit, check units
+import os
+import numpy as np
+from scipy.ndimage import rotate, shift
+
+# local imports
+import utils
+from read_dm3 import DM3
+
+#
+# User inputs
+#
+
+# Path to a DigitalMicrograph file containing a detector map (stacks will be averaged)
+detector_image_path = r"D:\OneDrive\OneDrive\Documents\PostDoc-TCD\Projects\pulse\pulse_paper\figures\data\how_flat\001_2710_JEOL ADF1.dm4"
+
+# name of detector, purely for plotting and output
+detector_name = "How flat is " + os.path.basename(detector_image_path)
+
+# shift (x, y) of image to get center of detector in middle (in integer pixels)
+center_shift = (0, 0)
+
+# Rotation of detector map if desired (in degrees)
+rotation = 0
+
+# Threshold sets fraction of dynamic range above which is considered active region
+threshold = 0.3
+
+#
+# Processing starts here
+#
+
+# Load the detector image
+det_image = DM3(detector_image_path).image.astype(np.float64)
+
+# If a stack, take the average through the stack
+if det_image.ndim > 2:
+    det_image = np.sum(det_image, axis=2)
+
+# get the number of pixels, just for convenience
+n = det_image.size
+
+# get a number to fill the background with for shifting/rotating (takes an average of the lowest 4% of pixels)
+n_low = int(0.04 * n)
+fill = np.mean(np.sort(det_image.ravel())[:n_low])
+
+# deciding contrast limits for determining active area (and for plotting)
+pcnt_min = 0.5
+pcnt_max = 0.1
+n_min = int(n * pcnt_min/100)
+n_max = int(n * pcnt_max/100)
+
+if n_max == 0:
+    n_max = 1
+
+vmn = np.sort(det_image.ravel())[n_min]
+vmx = np.sort(det_image.ravel())[-n_max]
+
+# calculate the image shift to get on center
+_xs = -int(center_shift[0])
+_ys = -int(center_shift[1])
+
+# shift to center
+det_new = shift(det_image, shift=(_ys, _xs), cval=fill)
+
+# rotate
+det_new = rotate(det_new, -rotation, cval=fill)
+
+# get mask for active area as 50% of dynamic range
+mask_new = det_new > vmn + (vmx - vmn) * threshold
+
+# calculate "how flat?" parameters
+# This will plot data, save plot and print out how flat parameters
+# Will aldo return parameters if further processing is wanted
+utils.calculate_how_flat(det_new, mask_new, f'{detector_name}.pdf')
---
+++ utils.py
@@ -0,0 +1,198 @@
+from skimage.transform import warp_polar
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.signal import savgol_filter, medfilt
+
+def to_polar(image):
+    # center is assumed to be center of image, we have already corrected this
+    polar = warp_polar(image)
+
+    # fig, axs = plt.subplots(1, 2)
+    # axs[0].imshow(image, cmap='magma')
+    # axs[1].imshow(polar, cmap='magma')
+    # plt.show()
+
+    return polar
+
+def calculate_background(image, mask):
+    # get fraction of background to account for detectoe edges etc
+    mn = image[np.logical_not(mask)].min()
+    mx = image[np.logical_not(mask)].max()
+    lvl = mn + (mx - mn) / 4
+
+    return np.mean(image, where=image < lvl)
+
+def calculate_active_average(image, mask):
+    return np.mean(image, where=mask)
+
+def find_ellipticity(inner_edge):
+
+    elp = 0
+
+    for i in range(inner_edge.shape[0]):
+
+        inds = [i, i + 90, i + 180, i + 270]
+        for ii in range(len(inds)):
+            if inds[ii] >= 360:
+                inds[ii] -= 360
+
+        axis_1 = inner_edge[inds[0]] + inner_edge[inds[2]]
+        axis_2 = inner_edge[inds[1]] + inner_edge[inds[3]]
+
+        _elp = axis_1 / axis_2
+        if _elp > elp:
+            elp = _elp
+
+    return elp
+
+def calculate_smoothness(image, mask, hist):
+    # method 1, find max then fwhm
+    hist_f = hist[0]
+    bin_width = hist[1][1] - hist[1][0]
+    max_i = np.argmax(hist_f)
+    above_hm = hist_f > hist_f[max_i] / 2
+
+    for i in range(max_i):
+        if not above_hm[max_i - i]:
+            break
+        low = max_i - i
+
+    for i in range(hist_f.size - max_i):
+        if not above_hm[max_i + i]:
+            break
+        high = max_i + i
+
+    fwhm_1 = (high - low) * bin_width
+
+    # # method 2, just stats
+    # # mean = np.mean(image, where=mask)
+    # std = np.std(image, where=mask)
+    # fwhm_2 = std * 2.355
+
+    return fwhm_1
+
+
+def calculate_how_flat(_det_image, _mask_image, fname=None):
+
+    det_image = _det_image.copy()
+    mask_image = _mask_image.copy()
+
+    det_background = calculate_background(det_image, mask_image)
+
+    det_image -= det_background
+
+    det_image /= calculate_active_average(det_image, mask_image)
+
+    det_polar = to_polar(det_image)
+    mask_polar = to_polar(mask_image).astype(bool) # this is where there is active detector
+    rad_mask = to_polar(np.ones_like(det_image)).astype(bool) # this is just where there is data
+
+    det_az = np.mean(det_polar, axis=1, where=mask_polar)
+
+    det_rad = np.mean(det_polar, axis=0, where=rad_mask)
+    mask_rad = np.mean(mask_polar, axis=0, where=rad_mask) > 0.9
+
+    det_hist_raw = np.histogram(det_image[mask_image], bins=50)
+
+    det_hist = medfilt(det_hist_raw[0]), det_hist_raw[1]
+
+    # calculate radius of inner
+    inner_r = np.argmax(mask_polar, axis=1)
+    inner_r_filt = savgol_filter(inner_r, 51, 3)
+
+    ellipticity = find_ellipticity(inner_r_filt)
+
+    if ellipticity == 0:
+
+        inner_r = np.argmax(np.logical_not(mask_polar), axis=1)
+        inner_r_filt = savgol_filter(inner_r, 51, 3)
+
+        ellipticity = find_ellipticity(inner_r_filt)
+
+
+    # calculate flatness
+    det_flatness = np.std(det_rad, where=mask_rad)
+
+    # calculate roundness
+    det_roundness = np.std(det_az)
+
+    # calculate smoothness
+    det_smoothness = calculate_smoothness(det_image, mask_image, det_hist)
+
+    print("Flatness")
+    print(f"Analog: {det_flatness}")
+    print("--------------------")
+
+    print("Roundness")
+    print(f"Analog: {det_roundness}")
+    print("--------------------")
+
+    print("Smoothness")
+    print(f"Analog: {det_smoothness}")
+    print("--------------------")
+
+    print("Ellipticity")
+    print(f"{(ellipticity - 1)}")
+    print("--------------------")
+
+
+    fig, axs = plt.subplots(2, 4)
+
+    axs[0, 0].imshow(mask_image * _det_image)
+    axs[0, 1].imshow(mask_image)
+
+    axs[0, 2].imshow(det_polar)
+    axs[0, 3].imshow(rad_mask + 2 * mask_polar)
+    axs[0, 3].plot(inner_r_filt, np.arange(det_polar.shape[0]), color='r')
+
+    # ellipticity
+    axs[1, 0].plot(inner_r)
+    axs[1, 0].plot(inner_r_filt)
+
+    # flatness
+    axs[1, 1].plot(det_rad)
+
+    rad_start = np.argmax(mask_rad)
+    rad_end = mask_rad.size - np.argmax(mask_rad[::-1]) - 1
+
+    axs[1, 1].axvline(x=rad_start, color = 'k')
+    axs[1, 1].axvline(x=rad_end, color = 'k')
+
+    # roundness
+    axs[1, 2].plot(det_az)
+
+    # smoothness
+
+    diffs = np.diff(det_hist_raw[1])
+
+    x = [det_hist_raw[1][0]]
+    y = [0]
+
+    i = 0
+    for c in range(diffs.size):
+        x.append(x[i])
+        y.append(det_hist_raw[0][c])
+
+        i += 1
+
+        x.append(x[i] + diffs[c])
+        y.append(y[i])
+
+        i += 1
+
+    x.append(x[i])
+    y.append(0)
+
+    axs[1, 3].plot(x, y)
+
+    #
+    # show plot
+    #
+
+    if fname is not None:
+        plt.savefig(f'{fname}')
+    plt.show()
+    plt.close()
+
+    return det_flatness, det_roundness, det_smoothness, ellipticity
+
